f3679a15f6c02a251bb092d846601c3950852e49
irpangeekseat <irpan.supendi@geekseat.com.au>
2020-05-09 20:15:03 +0700

    initial commit

---
+++ account/account.go
@@ -0,0 +1,11 @@
+//Account represent account entity model
+Account struct {
+	ID        string     `json:"id"`
+	Name      string     `json:"name"`
+	Email     string     `json:"email"`
+	Phone     string     `json:"phone"`
+	Password  string     `json:"-"`
+	CreatedAt time.Time  `json:"createdAt"`
+	UpdatedAt *time.Time `json:"updatedAt"`
+	DeletedAt *time.Time `json:"deletedAt"`
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/grow-entities-go
+
+go 1.14
